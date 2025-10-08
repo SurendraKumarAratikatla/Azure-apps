aab2e73dc9768ed103638482f467d8aa980de2c3
unknown <surendra.aratikatla1608@gmail.com>
2025-10-08 07:41:14 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 Hello from Surendra's Flask App on Azure Web App!"
+
+@app.route("/status")
+def status():
+    return {"status": "running", "app": "Flask on Azure"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
